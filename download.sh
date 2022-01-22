713f8a26693c0ec3885936d8f7cc147b5d23564a
erdiari <merdiari@gmail.com>
2022-01-22 14:12:12 +0300

    changed downloaded audio file to wav.

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yt-dlp -x --audio-format "opus" -a music-list.txt -o 'Music/%(title)s [%(id)s].%(ext)s'
+yt-dlp -x --audio-format "wav" -a music-list.txt -o 'Music/%(title)s [%(id)s].%(ext)s'
